6a217007570113f62b1f900ad43fd98f09b1fc02
jeanheberth <jeanheberth19@gmail.com>
2024-10-03 22:04:23 -0300

    criado os testes básicos solicitados

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
-const { defineConfig } = require("cypress");
+const {defineConfig} = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        specPattern: 'cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
+        supportFile: 'cypress/support/e2e.js',
+        baseUrl: 'https://qastage.buildbox.one/18/cadastro',
+        setupNodeEvents(on, config) {
+            allureWriter(on, config);
+            return config;
+        },
     },
-  },
+
+    "video": true,
+    "videosFolder": "cypress/videos",
+    "screenshotsFolder": "cypress/screenshots",
+    "viewportWidth": 1280,
+    "viewportHeight": 720,
+    "defaultCommandTimeout": 1000
 });
